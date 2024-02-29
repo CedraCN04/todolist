3928b5d2b097dbf5a4a4f56b7e7f29be382d0c8c
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-02-29 12:20:25 +0100

    Correction des bugs fonction edition

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
 import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
 type Task = {
@@ -12,12 +14,18 @@ type Task = {
   done: boolean;
 };
 
+// rechercher par le nom
+// créer un input pour rechercher le nom
+// créer une fonction qui va récupérer les informations entrées dans l'input
+// déclenche la recherche en cliquant sur un bouton.
+
+// afficher les todos suivant des critères =>  tout | complété | non completé
+
 export default function Home() {
   const [title, setTitle] = useState("");
-  const [currentTaskId, setCurrentTaskId] = useState("");
+  const [currentTaskId, setCurrentTaskId] = useState<string | null>(null);
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [openEditTask, setIsOpenEditTask] = useState(false);
-  const [isDone, setIsDone] = useState(false);
+  const [editTitle, setEditTitle] = useState("");
 
   const removeTask = (id: string) => {
     const newTasks = tasks.filter((task) => task.id !== id);
@@ -25,20 +33,20 @@ export default function Home() {
   };
 
   const editTask = (id: string) => {
-    setIsOpenEditTask(true);
     setCurrentTaskId(id);
+    const newTask = tasks.find((task) => task.id === id);
+    setEditTitle(newTask?.title ?? "");
   };
 
-  const editTaskTitle = (id: string, newTitle: string) => {
+  const editTaskTitle = () => {
     const newTasks = tasks.map((task) => {
       if (task.id === currentTaskId) {
-        return { ...task, title: newTitle };
+        return { ...task, title: editTitle };
       }
       return task;
     });
     setTasks(newTasks);
-    setIsOpenEditTask(false);
-    setTitle("");
+    setCurrentTaskId(null);
   };
 
   const addTask = (e: React.FormEvent) => {
@@ -65,6 +73,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-4 p-20">
       <h1 className="text-2xl my-10">Ma liste de tâches</h1>
+      <div className="flex flex-row items-center gap-2">
+        <Input placeholder="Rechercher une tâche" />
+        <Button>
+          <FaSearch />
+        </Button>
+      </div>
       <form
         className="flex flex-row items-center justify-around w-3/5"
         onSubmit={addTask}
@@ -78,54 +92,63 @@ export default function Home() {
           Ajoutez
         </Button>
       </form>
-      <ul className="w-3/5 my-10 flex flex-col justify-between items-center">
+      <ul className="w-3/5 my-10 flex flex-col items-center gap-4">
         {tasks.map((task) => (
           <li
             key={task.id}
-            className={`text-base border flex flex-row justify-between items-center border-gray-500 rounded-lg p-4 my-4 w-full ${
+            className={cn(
+              "text-base border flex flex-col items-center border-gray-500 rounded-lg p-4 w-full",
               task.done ? "line-through" : ""
-            }`}
+            )}
           >
-            <div className="flex flex-row items-center justify-between">
-              <input
-                type="checkbox"
-                checked={task.done}
-                onChange={() => toggleTaskDone(task.id)}
-                className="mr-4"
-              />
-              {task.title}
+            <div className="w-full flex flex-row items-center justify-between">
+              <div className="flex flex-row justify-between">
+                <input
+                  type="checkbox"
+                  checked={task.done}
+                  onChange={() => toggleTaskDone(task.id)}
+                  className="mr-4 cursor-pointer"
+                />
+                {task.title}
+              </div>
+              <div className="flex flex-row justify-between gap-4">
+                <Button
+                  className="bg-blue-400 hover:bg-blue-500"
+                  onClick={() => editTask(task.id)}
+                >
+                  Editer
+                </Button>
+                <Button
+                  className="bg-red-400 hover:bg-red-500"
+                  onClick={() => removeTask(task.id)}
+                >
+                  Supprimer
+                </Button>
+              </div>
             </div>
-            <div className="flex-flex-row justify-between items-center">
-              <Button
-                className="bg-blue-400 hover:bg-blue-500 mx-4"
-                onClick={() => editTask(task.id)}
-              >
-                Editer
-              </Button>
-              <Button
-                className="bg-red-400 hover:bg-red-500"
-                onClick={() => removeTask(task.id)}
-              >
-                Supprimer
-              </Button>
-            </div>
+            {task.id === currentTaskId && (
+              <div className="flex flex-row items-center justify-around w-3/5">
+                <Input
+                  placeholder="Modifier la tâche"
+                  value={editTitle}
+                  onChange={(e) => setEditTitle(e.target.value)}
+                />
+                <Button
+                  onClick={() => editTaskTitle()}
+                  className="mx-4 bg-green-400 hover:bg-green-500 text-black"
+                >
+                  Modifier
+                </Button>
+                <Button
+                  onClick={() => setCurrentTaskId(null)}
+                  className="mx-4 bg-red-400 hover:bg-red-500 text-black"
+                >
+                  Annuler
+                </Button>
+              </div>
+            )}
           </li>
         ))}
-        {openEditTask && (
-          <div className="flex flex-row items-center justify-around w-3/5">
-            <Input
-              placeholder="Modifier la tâche"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            <Button
-              onClick={() => editTaskTitle(tasks[0].id, title)}
-              className="mx-4 bg-green-400 hover:bg-green-500 text-black"
-            >
-              Modifier
-            </Button>
-          </div>
-        )}
       </ul>
     </main>
   );
