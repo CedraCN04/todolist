e62eb54df1dd42e93a91111ee7c8a49cdf3dc8e3
GuillaumeCeccoli <guiceccoli@gmail.com>
2024-04-21 23:15:12 +0200

    Retrait d'un hook inutile

--- lib/hooks/getUser.ts
+++
@@ -1,9 +0,0 @@
-// recupérer le user_id depuis la base de données
-
-import { createClientServer } from "../supabase/server";
-
-export const useGetUser = async (user_id: string) => {
-  const supabase = createClientServer();
-  const { data } = await supabase.from("tasksList").select();
-  return data;
-}
